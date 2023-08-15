a3af334d01f08ea565b231523adb053578fa33e5
sadiq <sadiq_2001@mail.ru>
2023-08-15 11:52:21 +0400

    fixed email error

--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
